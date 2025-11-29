979674968dd81f3cef562c5da85e01190f213b62
Ahmet Akif Arslan <ahmet.akif@lugath.com>
2025-11-29 13:15:32 +0300

    404 links bugfix

--- src/_data/i18n.js
+++
@@ -1,7 +0,0 @@
-const { SUPPORTED_LOCALES, DEFAULT_LOCALE, loadTranslationFiles } = require('../../lib/i18n');
-
-module.exports = () => ({
-  locales: SUPPORTED_LOCALES,
-  defaultLocale: DEFAULT_LOCALE,
-  translations: loadTranslationFiles(),
-});
